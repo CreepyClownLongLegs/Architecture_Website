d7600c180296742570449006d783bd389eac2775
e11927096 <e11927096@student.tuwien.ac.at>
2023-10-18 01:50:25 +0200

    cat update

--- architectureBlog/src/app/app.component.ts
+++ architectureBlog/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'architectureBlog';
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    
+    const targets = document.querySelectorAll('img');
+
+    targets.forEach((img: HTMLImageElement) => {
+      console.log('💩');
+      const rect = img.getBoundingClientRect().top;
+      if (rect <= window.innerHeight) {
+        const src = img.getAttribute('data-lazy');
+        if (src) { // Check if src is not null
+          img.setAttribute('src', src);
+          img.classList.add('fade');
+        }
+      }
+    });
+  }
 }
--- architectureBlog/src/app/header/header.component.ts
+++ architectureBlog/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  backgroundColor = '#FFEEEE'; // Initial background color
+  backgroundColor = 'white'; // Initial background color
   fontColot = 'black';
+  borderBottom = 'black';
   lastBackgroundColor = this.backgroundColor; // Store the last color
 
   onMouseEnter() {
@@ -19,6 +20,22 @@ export class HeaderComponent {
   onMouseLeave() {
     this.backgroundColor = this.lastBackgroundColor;
     this.fontColot = 'black';
+  }
+
+  lastScrollTop = 0;
+  headerVisible = true;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > this.lastScrollTop) {
+      // Scrolling down, hide the header
+      this.headerVisible = false;
+    } else {
+      // Scrolling up, show the header
+      this.headerVisible = true;
+    }
+    this.lastScrollTop = st;
   }
 
 }
